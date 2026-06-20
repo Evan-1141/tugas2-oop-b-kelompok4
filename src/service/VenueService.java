e353979a3d183af2904c31c788464bf43f9fbe72
TheNightShadeee <thenightshadeee@gmail.com>
2026-06-20 23:34:30 +0800

    Update Service Layer

--- src/service/VenueService.java
+++ src/service/VenueService.java
@@ -2,4 +2,11 @@ package service;
 
 public class VenueService {
     
+    public Object getAllTickets(){}
+
+    public Object getTicketById(String id){}
+
+    public Object buyTicket(){}
+
+    public Object refundTicket(String id){}
 }
