ae778997b87d1081e13dcbed337885efc5946fc5
idabagusweda <astawa.2505551123@student.unud.ac.id>
2026-06-21 16:38:48 +0800

    feat(model): implementasikan entity User sesuai database schema

--- src/model/User.java
+++ src/model/User.java
@@ -1,5 +1,79 @@
 package model;
 
 public class User {
-    
+
+    private String id;
+    private String name;
+    private String email;
+    private String phone;
+    private String role;
+    private String createdAt;
+
+    public User(String id, String name, String email,
+                String phone, String role, String createdAt) {
+
+        this.id = id;
+        this.name = name;
+        this.email = email;
+        this.phone = phone;
+        this.role = role;
+        this.createdAt = createdAt;
+    }
+
+    // Getter
+
+    public String getId() {
+        return id;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public String getPhone() {
+        return phone;
+    }
+
+    public String getRole() {
+        return role;
+    }
+
+    public String getCreatedAt() {
+        return createdAt;
+    }
+
+    // Setter
+
+    public void setId(String id) {
+        this.id = id;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public void setPhone(String phone) {
+        this.phone = phone;
+    }
+
+    public void setRole(String role) {
+        this.role = role;
+    }
+
+    public void setCreatedAt(String createdAt) {
+        this.createdAt = createdAt;
+    }
+
+    @Override
+    public String toString() {
+        return name + " (" + role + ")";
+    }
 }
